884958aa11d00d11f2a1250f3fe125f916e4bcaf
Sprezz <arthurnalves@gmail.com>
2022-03-20 11:27:39 +0100

    Add random fake name

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Typescript module
+https://socket.io/get-started/chat
 
 Chat proof of concept
+
+Run with:
+npx nodemon src/index.ts -e html,ts
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-const express = require('express');
+import * as express from 'express';
+import * as http from 'http';
+import { faker } from '@faker-js/faker';
+import { Server } from "socket.io";
+
 const app = express();
-const http = require('http');
+
 const server = http.createServer(app);
-const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req: any, res: any) => {
@@ -10,14 +13,14 @@ app.get('/', (req: any, res: any) => {
 });
 
 io.on('connection', (socket: any) => {
+    socket.name = faker.name.findName();
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-        console.log('message: ' + msg);
+        io.emit('chat message', `${socket.name}: ${msg}`);
     });
 });
 
